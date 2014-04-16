cd7fcf1ae9ec572fdff674e5ef8f15aca412e667
Andrew Sykes <ajsykes92@gmail.com>
2014-04-16 13:57:26 -0500

    initial commit

---
+++ src/com/app/groupalarm/MainActivity.java
@@ -0,0 +1,54 @@
+package com.app.groupalarm;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.facebook.Session;
+
+public class MainActivity extends Activity {
+	
+	public WebView webview;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		webview = (WebView) findViewById(R.id.webview);
+		webview.setWebViewClient(new WebViewClient());
+		
+		
+		//Enable JS
+		WebSettings webSettings = webview.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		webSettings.setAllowFileAccessFromFileURLs(true);
+		
+		webview.setWebChromeClient(new WebChromeClient());
+		
+		//Enable access to all methods in JS within WebAppInterface
+		webview.addJavascriptInterface(new WebAppInterface(this, webview), "Android");
+		
+		webview.loadUrl("file:///android_asset/dist/index.html");
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	      super.onActivityResult(requestCode, resultCode, data);
+	      Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+	  }
+
+}
---
+++ src/com/app/groupalarm/WebAppInterface.java
@@ -0,0 +1,54 @@
+package com.app.groupalarm;
+
+import com.facebook.Session;
+import com.facebook.Session.StatusCallback;
+import com.facebook.SessionState;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+
+public class WebAppInterface {
+	
+	Context mContext;
+	String token;
+	WebView view;
+	
+	public WebAppInterface(Context c, WebView v){
+		
+		mContext = c;
+		view = v;
+	}
+	
+	@JavascriptInterface
+	public void facebookLogin(){
+		
+		Log.i("Facebook Login Call", "HELLO");
+		// start Facebook Login
+	    Session.openActiveSession((Activity) mContext, true, new StatusCallback() {
+			
+			@Override
+			public void call(Session session, SessionState state, Exception exception) {
+				if(session.isOpened()){
+					MainActivity main = (MainActivity) mContext;
+					Log.i("FB Success", "Success");
+					main.webview.loadUrl("javascript:processToken('" + session.getAccessToken() + "')");
+				}
+			}
+		});
+	    
+	}
+	
+	@JavascriptInterface
+	private String getToken(){
+		Log.i("Token", Session.getActiveSession().getAccessToken());
+		return token;
+	}
+	
+	private void setToken(String s){
+		token = s;
+	}
+	
+}
